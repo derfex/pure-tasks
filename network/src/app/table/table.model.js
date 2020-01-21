06081ad4f0967b0157a52f447fccb63823650b11
derfex <derfex@yandex.ru>
2020-01-21 16:42:09 +0300

    feature (network): use `fetch`, `response.json()`

--- network/src/app/table/table.model.js
+++ network/src/app/table/table.model.js
@@ -1,8 +1,10 @@
-import { commits } from './commits.data.js';
-
 async function requestCommits() {
-    // Same as `return commits;`.
-    return await new Promise((resolve) => resolve(commits));
+    const url = 'https://api.github.com/repos/derfex/pure-tasks/commits';
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('HTTP error, status = ' + response.status);
+    }
+    return await response.json();
 }
 
 
