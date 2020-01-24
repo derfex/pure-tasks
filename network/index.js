fed498b3512f30b2333c92c333cbac6463353065
derfex <derfex@yandex.ru>
2020-01-24 15:02:52 +0300

    Perf (network): init modules in parallel

--- network/index.js
+++ network/index.js
@@ -3,6 +3,16 @@ import { initUserModule } from './src/app/user/user.module.js';
 
 
 export async function start() {
-    await initCommitsModule(document.getElementById('commits-table'));
-    await initUserModule(document.getElementById('user-container'));
+    const promises = [];
+    promises.push(initCommitsModule(document.getElementById('commits-table')));
+    promises.push(initUserModule(document.getElementById('user-container')));
+    const results = await Promise.allSettled(promises);
+
+    let i = -1;
+    const limit = results.length;
+    while (++i < limit) {
+        if (results[i].status === 'rejected') {
+            console.error(results[i].reason);
+        }
+    }
 }
