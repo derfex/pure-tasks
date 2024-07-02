6239ef6d4d37afd71a530ccb16e6d823fb07864b
derfex <derfex@yandex.ru>
2024-07-03 02:47:54 +0300

    5. Write `checkIfTextsAreAnagrams` (#6)
    * Feature (anagram): Implement the check
    
    * Docs (README.md): Update the list

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Pure tasks
+
 Set of front-end tasks on native web technologies.
 Compilers, libraries, project builders, obfuscators, etc. are not used.
 
-* [Binary search](https://derfex.github.io/pure-tasks/binary-search/test/)
-* [Tic-tac-toe](https://derfex.github.io/pure-tasks/tic-tac-toe/)
-* [SVG chart](https://derfex.github.io/pure-tasks/svg-chart/)
+* [Binary search](https://derfex.github.io/pure-tasks/binary-search/test)
+* [Check if texts are anagrams](https://derfex.github.io/pure-tasks/check-if-texts-are-anagrams)
+* [SVG chart](https://derfex.github.io/pure-tasks/svg-chart)
+* [Tic-tac-toe](https://derfex.github.io/pure-tasks/tic-tac-toe)
---
+++ check-if-texts-are-anagrams/src/anagram/anagram-non-optimized.js
@@ -0,0 +1,49 @@
+(function() {
+  'use strict';
+
+  window.module.AnagramNonOptimized = (function() {
+    /**
+     * A set of functions for checking if two texts are anagrams of each other.
+     */
+    const Anagram = Object.create(null);
+
+    /**
+     * Check if two texts are anagrams of each other.
+     * @param {string} text1 — the first text to comparison.
+     * @param {string} text2 — the second text to comparison.
+     * @returns {boolean}
+     */
+    Anagram.checkIfTextsAreAnagrams = function(text1, text2) {
+      const normalizedText1 = normalizeText(text1);
+      const normalizedText2 = normalizeText(text2);
+      return normalizedText1 === normalizedText2;
+    };
+
+    const allowedSymbolsSet = (() => {
+      const countOfLetters = 26;
+      const start = 'a'.charCodeAt(0);
+      const end = start + countOfLetters;
+      const set = new Set();
+      for (let i = start; i < end; i++) {
+        set.add(String.fromCharCode(i));
+      }
+      return set;
+    })();
+
+    /**
+     * Generate a text with sorted allowed symbols.
+     * @param {string} text — a text for normalization.
+     * @returns {string}
+     */
+    function normalizeText(text) {
+      return text
+        .split('')
+        .map(symbol => symbol.toLowerCase())
+        .filter(symbol => allowedSymbolsSet.has(symbol))
+        .sort()
+        .join('');
+    }
+
+    return Anagram
+  })();
+})();
