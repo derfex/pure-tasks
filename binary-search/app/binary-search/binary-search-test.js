2c5ec327aa9496adb4927890d1e251e63c29c0d6
derfex <derfex@yandex.ru>
2025-01-27 00:52:18 +0300

    #11. Refactor (binary-search): Translate to English

--- binary-search/app/binary-search/binary-search-test.js
+++ binary-search/app/binary-search/binary-search-test.js
@@ -1,88 +1,95 @@
-'use strict';
+(function() {
+  'use strict';
 
-const TEST_DATA = [
-    // Тестировать примитивный набор параметров.
-    {
-        parameters: [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7],
-        result: 6,
-    },
+  // # Modules: `import` emulation
+  const { BinarySearch } = window.module.Search;
 
-    // Тестировать незакономерный упорядоченный массив, результат на первой итерации.
-    {
-        parameters: [[0, 23, 175, 312, 509, 856, 4098, 7931, 10000], 509],
-        result: 4,
-    },
 
-    // Тестировать незакономерный упорядоченный массив, результат на последней итерации.
+  // # Configuration
+  const testCases = [
+    // Test a primitive set of parameters.
     {
-        parameters: [[0, 23, 175, 312, 509, 856, 4098, 7931, 10000], 856],
-        result: 5,
+      parameters: [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7],
+      result: 6,
     },
 
-    // Тестировать массивы с дробными значениями.
+    // Test an irregular sorted array, result on the first iteration.
     {
-        parameters: [[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], 2],
-        result: -1,
-    },
-    {
-        parameters: [[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], 0.5],
-        result: 4,
+      parameters: [[0, 23, 175, 312, 509, 856, 4098, 7931, 10000], 509],
+      result: 4,
     },
 
-    // Тестировать массивы с отрицательными значениями.
+    // Test an irregular sorted array, result on the last iteration.
     {
-        parameters: [[-100, -20, -3, 4, 50], -20],
-        result: 1,
-    },
-    {
-        parameters: [[-100, -20, -3, 4, 50], -1],
-        result: -1,
+      parameters: [[0, 23, 175, 312, 509, 856, 4098, 7931, 10000], 856],
+      result: 5,
     },
 
-    // Тестировать пустой массив.
+    // Test arrays with fractional values.
     {
-        parameters: [[], 1],
-        result: -1,
+      parameters: [[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], 2],
+      result: -1,
+    },
+    {
+      parameters: [[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], 0.5],
+      result: 4,
     },
 
-    // Тестировать массивы с малым количеством элементов.
+    // Test arrays with negative values.
     {
-        parameters: [[1], 1],
-        result: 0,
+      parameters: [[-100, -20, -3, 4, 50], -20],
+      result: 1,
     },
     {
-        parameters: [[1], 10],
-        result: -1,
+      parameters: [[-100, -20, -3, 4, 50], -1],
+      result: -1,
     },
-    {
-        parameters: [[1, 2], 1],
-        result: 0,
-    },
-    {
-        parameters: [[1, 2], 10],
-        result: -1,
-    },
-    {
-        parameters: [[1, 2, 3], 1],
-        result: 0,
-    },
-    {
-        parameters: [[1, 2, 3], 10],
-        result: -1,
-    },
-];
 
-const binarySearch = window.module.Search.BinarySearch.findIndex;
+    // Test arrays with negative values.
+    {
+      parameters: [[], 1],
+      result: -1,
+    },
 
-let failedTestsCount = 0;
-TEST_DATA.forEach((data, testIndex) => {
-    const testResult = binarySearch.apply(null, data.parameters) === data.result;
+    // Test arrays with a small number of elements.
+    {
+      parameters: [[1], 1],
+      result: 0,
+    },
+    {
+      parameters: [[1], 10],
+      result: -1,
+    },
+    {
+      parameters: [[1, 2], 1],
+      result: 0,
+    },
+    {
+      parameters: [[1, 2], 10],
+      result: -1,
+    },
+    {
+      parameters: [[1, 2, 3], 1],
+      result: 0,
+    },
+    {
+      parameters: [[1, 2, 3], 10],
+      result: -1,
+    },
+  ];
+
+
+  // # Run the tests
+  let failedTestsCount = 0;
+  testCases.forEach((data, testIndex) => {
+    const testResult = BinarySearch.findIndex.apply(null, data.parameters) === data.result;
     if (!testResult) {
-        ++failedTestsCount;
+      ++failedTestsCount;
     }
     console.assert(
-        testResult,
-        'Индекс теста:', testIndex, 'Параметры:', data,
+      testResult,
+      'Test index:', testIndex, 'Parameters:', data,
     );
-});
-console.info('Количество непройденных тестов:', failedTestsCount);
+  });
+  console.info('Number of failed tests:', failedTestsCount);
+})();
