a667e967929fda413c9b6f3589409ebe772eea02
derfex <derfex@yandex.ru>
2025-01-27 03:58:54 +0300

    #11. Refactor (binary-search): Rewrite to the static method (#12)

---
+++ binary-search/app/binary-search/binary-search-test.js
@@ -0,0 +1,92 @@
+(function () {
+  'use strict';
+
+  // # Modules: `import` emulation
+  const { BinarySearch } = window.module.Search;
+
+
+  // # Configuration
+  const testCases = [
+    // Test a primitive set of parameters.
+    {
+      parameters: [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7],
+      result: 6,
+    },
+
+    // Test an irregular sorted array, result on the first iteration.
+    {
+      parameters: [[0, 23, 175, 312, 509, 856, 4098, 7931, 10000], 509],
+      result: 4,
+    },
+
+    // Test an irregular sorted array, result on the last iteration.
+    {
+      parameters: [[0, 23, 175, 312, 509, 856, 4098, 7931, 10000], 856],
+      result: 5,
+    },
+
+    // Test arrays with fractional values.
+    {
+      parameters: [[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], 2],
+      result: -1,
+    },
+    {
+      parameters: [[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], 0.5],
+      result: 4,
+    },
+
+    // Test arrays with negative values.
+    {
+      parameters: [[-100, -20, -3, 4, 50], -20],
+      result: 1,
+    },
+    {
+      parameters: [[-100, -20, -3, 4, 50], -1],
+      result: -1,
+    },
+
+    // Test arrays with negative values.
+    {
+      parameters: [[], 1],
+      result: -1,
+    },
+
+    // Test arrays with a small number of elements.
+    {
+      parameters: [[1], 1],
+      result: 0,
+    },
+    {
+      parameters: [[1], 10],
+      result: -1,
+    },
+    {
+      parameters: [[1, 2], 1],
+      result: 0,
+    },
+    {
+      parameters: [[1, 2], 10],
+      result: -1,
+    },
+    {
+      parameters: [[1, 2, 3], 1],
+      result: 0,
+    },
+    {
+      parameters: [[1, 2, 3], 10],
+      result: -1,
+    },
+  ];
+
+
+  // # Run the tests
+  let failedTestsCount = 0;
+  testCases.forEach((data, testIndex) => {
+    const testResult = BinarySearch.findIndex.apply(null, data.parameters) === data.result;
+    if (!testResult) {
+      ++failedTestsCount;
+    }
+    console.assert(testResult, 'Test index:', testIndex, 'Parameters:', data);
+  });
+  console.info('Number of failed tests:', failedTestsCount);
+})();
---
+++ binary-search/app/binary-search/binary-search.js
@@ -0,0 +1,37 @@
+(function () {
+  'use strict';
+
+  window.module.Search = (function () {
+    class BinarySearch {
+      /**
+       * Find the index of the element `needle` with the desired value in the array using the binary search method.
+       * The array `haystack` must contain a set of decimal numbers sorted in ascending order.
+       * @param {readonly number[]} haystack — target array.
+       * @param {number} needle — required value.
+       * @returns {number} — index of the element with the desired value in the target array.
+       */
+      static findIndex(haystack, needle) {
+        let start = 0;
+        let end = haystack.length - 1;
+        do {
+          // Get the median index using overflow protection method.
+          const middle = Math.floor(start + (end - start) / 2);
+          const value = haystack[middle];
+          if (value === needle) {
+            return middle;
+          }
+          if (value < needle) {
+            start = middle;
+          } else {
+            end = middle;
+          }
+        } while (end - start > 1);
+        return haystack[end] === needle ? end : haystack[start] === needle ? start : -1;
+      }
+    }
+
+    const Search = Object.create(null);
+    Search.BinarySearch = BinarySearch;
+    return Search;
+  })();
+})();
