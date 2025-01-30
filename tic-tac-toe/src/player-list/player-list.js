af50cb59ed3ce5cf484fd4ba669493fa5b7723de
derfex <derfex@yandex.ru>
2025-01-31 02:14:14 +0300

    #15. Refactor (PlayerList): Convert `PlayerList` to the `class`

--- tic-tac-toe/src/player-list/player-list.js
+++ tic-tac-toe/src/player-list/player-list.js
@@ -2,45 +2,47 @@
   'use strict';
 
   window.module.PlayerList = (() => {
-    /**
-     * Класс, позволяющий определить данные текущего игрока.
-     * @param {object[]} dataPlayers — данные об игроках.
-     * @constructor
-     */
-    const PlayerList = function(dataPlayers) {
-      this._DATA_PLAYERS = dataPlayers.slice();
-      this._DATA_PLAYERS.forEach(player => {
-        Object.freeze(player);
-      });
+    class PlayerList {
+      /**
+       * Класс, позволяющий определить данные текущего игрока.
+       * @param {object[]} dataPlayers — данные об игроках.
+       * @constructor
+       */
+      constructor(dataPlayers) {
+        this._DATA_PLAYERS = dataPlayers.slice();
+        this._DATA_PLAYERS.forEach(player => {
+          Object.freeze(player);
+        });
 
-      this._LIMIT = this._DATA_PLAYERS.length - 1;
+        this._LIMIT = this._DATA_PLAYERS.length - 1;
 
-      this._currentPlayerIndex = this._LIMIT;
-    };
-
-    /**
-     * Получить следующего игрока.
-     * @returns {Object}
-     */
-    PlayerList.prototype.getNext = function() {
-      let index = this._currentPlayerIndex + 1;
-      if (index > this._LIMIT) {
-        index = 0;
+        this._currentPlayerIndex = this._LIMIT;
       }
-      this._currentPlayerIndex = index;
-      return this._DATA_PLAYERS[index];
-    };
 
-    /**
-     * Вернуть предыдущее состояние.
-     */
-    PlayerList.prototype.revert = function() {
-      let index = this._currentPlayerIndex - 1;
-      if (index < 0) {
-        index = this._LIMIT;
+      /**
+       * Получить следующего игрока.
+       * @returns {Object}
+       */
+      getNext() {
+        let index = this._currentPlayerIndex + 1;
+        if (index > this._LIMIT) {
+          index = 0;
+        }
+        this._currentPlayerIndex = index;
+        return this._DATA_PLAYERS[index];
       }
-      this._currentPlayerIndex = index;
-    };
+
+      /**
+       * Вернуть предыдущее состояние.
+       */
+      revert() {
+        let index = this._currentPlayerIndex - 1;
+        if (index < 0) {
+          index = this._LIMIT;
+        }
+        this._currentPlayerIndex = index;
+      }
+    }
 
     const module = Object.create(null);
     module.PlayerList = PlayerList;
