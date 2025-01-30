85384c395816d271fc70a8da7de0c3eea9a0bb65
derfex <derfex@yandex.ru>
2025-01-31 02:37:33 +0300

    #15. Refactor (PlayingField): Apply the code style

--- tic-tac-toe/src/playing-field/playing-field.js
+++ tic-tac-toe/src/playing-field/playing-field.js
@@ -1,45 +1,45 @@
-(function() {
-    'use strict';
+(() => {
+  'use strict';
 
-    window.module.PlayingField = (function() {
-        /**
-         * Набор функций для создания игрового поля.
-         */
-        const PlayingField = Object.create(null);
+  window.module.PlayingField = (() => {
+    /**
+     * Набор функций для создания игрового поля.
+     */
+    const PlayingField = Object.create(null);
 
-        const createRow = function createRow(size) {
-            const elementTR = document.createElement('tr');
-            for (let i = 0; i < size; i++) {
-                const elementTD = document.createElement('td');
-                elementTR.appendChild(elementTD);
-            }
-            return elementTR;
-        };
+    const createRow = size => {
+      const elementTR = document.createElement('tr');
+      for (let i = 0; i < size; i++) {
+        const elementTD = document.createElement('td');
+        elementTR.appendChild(elementTD);
+      }
+      return elementTR;
+    };
 
-        /**
-         * Создать игровое поле в виде пустой таблицы.
-         * @param {number} size — размер таблицы (количество ячеек по горизонтали и вертикали).
-         * @returns {HTMLTableElement}
-         */
-        PlayingField.createTable = function(size) {
-            const elementTable = document.createElement('table');
-            for (let i = 0; i < size; i++) {
-                const elementTR = createRow(size);
-                elementTable.appendChild(elementTR);
-            }
-            return elementTable;
-        };
+    /**
+     * Создать игровое поле в виде пустой таблицы.
+     * @param {number} size — размер таблицы (количество ячеек по горизонтали и вертикали).
+     * @returns {HTMLTableElement}
+     */
+    PlayingField.createTable = size => {
+      const elementTable = document.createElement('table');
+      for (let i = 0; i < size; i++) {
+        const elementTR = createRow(size);
+        elementTable.appendChild(elementTR);
+      }
+      return elementTable;
+    };
 
-        /**
-         * Отметить ячейку игрового поля.
-         * @param {HTMLTableCellElement} elementCell — указатель на DOM-element.
-         * @param {string} color — цвет фона.
-         */
-        PlayingField.markCell = function(elementCell, color) {
-            elementCell.style.backgroundColor = color;
-            elementCell.style.cursor = 'not-allowed';
-        };
+    /**
+     * Отметить ячейку игрового поля.
+     * @param {HTMLTableCellElement} elementCell — указатель на DOM-element.
+     * @param {string} color — цвет фона.
+     */
+    PlayingField.markCell = (elementCell, color) => {
+      elementCell.style.backgroundColor = color;
+      elementCell.style.cursor = 'not-allowed';
+    };
 
-        return PlayingField
-    })();
+    return PlayingField;
+  })();
 })();
