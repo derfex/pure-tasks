e99d8d3cc267bb23c72d511e4ccbd4e7a440b4f3
derfex <derfex@yandex.ru>
2025-02-02 22:57:37 +0300

    #19. Feature (Scene1): Display `bacteria-3`

--- gaming-banner/src/app/scene-1/scene-1.js
+++ gaming-banner/src/app/scene-1/scene-1.js
@@ -38,6 +38,9 @@
         <div class="app-scene-1__bacteria-2-container">
           <img class="app-scene-1__bacteria-2" alt="bacteria 2" src="${imagesPath}/bacteria-2.webp">
         </div>
+        <div class="app-scene-1__bacteria-3-container">
+          <img class="app-scene-1__bacteria-3" alt="bacteria 3" src="${imagesPath}/bacteria-3.webp">
+        </div>
       `;
 
         return element;
